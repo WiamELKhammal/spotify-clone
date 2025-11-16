2a309e3747ab9bf7d6e2184d543b6988229eba0e
Wiam EL Khammal <162513652+WiamELKhammal@users.noreply.github.com>
2025-11-17 00:34:08 +0100

    added model user

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    clerk: {
+        type: String,
+        required : true,
+        unique : true,
+    },
+},{timestamps: true});
